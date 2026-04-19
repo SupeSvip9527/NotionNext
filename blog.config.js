770bdb61fa11be69dbb1fff7b5dbe11a6153e535
SupeSvip9527 <ikarosdragonlady@gmail.com>
2026-04-19 20:42:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -5,7 +5,7 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://tanghh.notion.site/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
-     'c9eb74f217ba83f7a69c81f7436545c7',
+      'c9eb74f217ba83f7a69c81f7436545c7,en:c9eb74f217ba83f7a69c81f7436545c7',
   THEME: process.env.NEXT_PUBLIC_THEME || 'next', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 1991, // e.g if leave this empty, current year will be used.
